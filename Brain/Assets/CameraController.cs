85e9627ed8b425b5e341f9361ad48c9b8b2311c7
Roman <pencilking2002@gmail.com>
2015-11-21 17:34:54 -0500

    First project commit

---
+++ Brain/Assets/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	public float distAway = 2.0f;
+	
+	private Transform player;
+	private Transform follow;
+	private Vector3 targetPos;
+	private Vector3 vel;
+
+	
+	private void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		follow = GameObject.FindGameObjectWithTag("Follow").transform;
+	}
+	
+	private void LateUpdate ()
+	{
+		//targetPos = Vector3.SmoothDamp (transform.position, follow.position + follow.up * distAway, ref vel, 10.0f * Time.deltaTime);
+		targetPos = follow.position + follow.forward * -distAway;
+		
+		transform.position = targetPos;
+		
+		//transform.LookAt(Vector3.zero);
+		transform.rotation = Quaternion.LookRotation(follow.forward);
+		transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, follow.localEulerAngles.z + 180);
+	}
+}
---
+++ Brain/Assets/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+	
+	private Transform player;
+	public float speed = 10.0f;
+	
+	private Vector3 vel;
+		
+	private void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	private void Update ()
+	{
+		transform.position = Vector3.SmoothDamp (transform.position, player.position, ref vel, speed);
+		transform.LookAt(Vector3.zero);	
+	}
+}
